ba6839edc18a08206a37901bf6983cabd6bcbe51
Hoarfroster <hoarfroster@outlook.com>
2025-03-06 12:58:14 +0800

    Initialize the project with CRACO

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {HashRouter, Routes} from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <HashRouter>
+                <Routes>
+                </Routes>
+            </HashRouter>
+        </>
+    );
+}
+
+export default App;
